f976e2de54e513c74929b25224e1b9c5c0081a66
Matt Savoie <matthew.savoie@colorado.edu>
2025-06-02 13:57:11 -0600

    DAS-2347: clarify comments on CRS defaults

--- harmony_regridding_service/message_utilities.py
+++ harmony_regridding_service/message_utilities.py
@@ -97,6 +97,6 @@ def is_geographic_crs(crs_string: str) -> bool:
 
 
 def target_crs_from_message(message: Message) -> CRS:
-    """Return the message's crs or one from 4326."""
+    """Return the message's CRS or default to one from EPSG::4326."""
     target_crs = rgetattr(message, 'format.crs')
     return CRS(target_crs or 'EPSG:4326')
