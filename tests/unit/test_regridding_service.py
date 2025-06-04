1436903f77b75e21de24b7b76ab5e0b44f098696
Matt Savoie <matthew.savoie@colorado.edu>
2025-06-04 10:11:47 -0600

    DAS-2347: Fix end to end test.
    The old logic for testing for grid parts is no longer valid, and the updated
    test needed to include scale extents now.

--- tests/unit/test_regridding_service.py
+++ tests/unit/test_regridding_service.py
@@ -10,33 +10,42 @@ from harmony_service_lib.message import Source as HarmonySource
 
 from harmony_regridding_service.regridding_service import regrid
 
+test_scale_extent = {
+    'x': {'min': -180, 'max': 180},
+    'y': {'min': -90, 'max': 90},
+}
+
 
 @pytest.mark.parametrize(
-    'width, height, expected_width, expected_height',
+    'width, height, scale_extent, expected_width, expected_height, description',
     [
-        (100, 50, 100, 50),  # Enough grid parameters are provided.
-        (None, None, 5, 6),  # Grid parameters need to be created.
+        (100, 50, test_scale_extent, 100, 50, 'Grid parameters are provided.'),
+        (None, None, None, 5, 6, 'Grid parameters are excluded from message.'),
     ],
 )
 def test_regrid_projected_data_end_to_end(
-    width, height, expected_width, expected_height, smap_projected_netcdf_file, tmp_path
+    width,
+    height,
+    scale_extent,
+    expected_width,
+    expected_height,
+    description,
+    smap_projected_netcdf_file,
+    tmp_path,
 ):
     """Test the full regrid process for projected input data."""
     input_filename = str(smap_projected_netcdf_file)
     output_filename = str(tmp_path / 'regridded_output.nc')
     logger_mock = MagicMock()
 
-    # Define a target CRS and grid (example: Geographic WGS84)
+    # Define a target CRS [and optionally grid parameters]
     params = {
         'format': {
             'mime': 'application/x-netcdf',
             'crs': 'EPSG:4326',
             'width': width,
             'height': height,
-            'scaleExtent': {
-                'x': {'min': -180, 'max': 180},
-                'y': {'min': -90, 'max': 90},
-            },
+            'scaleExtent': scale_extent,
         },
         'sources': [{'collection': 'C123-TEST', 'shortName': 'SPL4SMAU'}],
     }
@@ -54,20 +63,20 @@ def test_regrid_projected_data_end_to_end(
     assert Path(output_filename).exists()
 
     with xr.open_datatree(output_filename) as ds_out:
-        assert 'crs' in ds_out
+        assert 'crs' in ds_out, description
 
-        assert ds_out.dims['y'] == expected_height
-        assert ds_out.dims['x'] == expected_width
+        assert ds_out.dims['y'] == expected_height, description
+        assert ds_out.dims['x'] == expected_width, description
 
-        assert 'sm_profile_forecast' in ds_out['Forecast_Data']
-        assert 'sm_profile_analysis' in ds_out['Analysis_Data']
-        assert 'tb_v_obs' in ds_out['Observations_Data']
+        assert 'sm_profile_forecast' in ds_out['Forecast_Data'], description
+        assert 'sm_profile_analysis' in ds_out['Analysis_Data'], description
+        assert 'tb_v_obs' in ds_out['Observations_Data'], description
 
         assert (
             ds_out['/Metadata/DatasetIdentification'].attrs['shortName'] == 'SPL4SMAU'
-        )
+        ), description
 
         assert (
             ds_out['Observations_Data/tb_v_obs'].attrs['long_name']
             == 'Composite resolution observed (L2_SM_AP or L1C_TB) V-pol ...'
-        )
+        ), description
