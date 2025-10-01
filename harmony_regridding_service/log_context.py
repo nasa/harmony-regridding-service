c4c15b5ac139698acbe1569c2a1f189e12ccb368
Matt Savoie <matthew.savoie@colorado.edu>
2025-10-01 17:35:03 -0600

    DAS-2382: Move Logging context initiation.

--- harmony_regridding_service/log_context.py
+++ harmony_regridding_service/log_context.py
@@ -6,15 +6,15 @@ Logging context from harmony is on a per request basis and adds a user and reque
 
 from logging import getLogger
 
-_logger = None
+_LOGGER = None
 
 
 def set_logger(logger):
     """Set the logger for this request."""
     global _logger
-    _logger = logger
+    _LOGGER = logger
 
 
 def get_logger(default_name='harmony-service.regridder'):
     """Get the context logger or fall back to module logger."""
-    return _logger if _logger else getLogger(default_name)
+    return _LOGGER if _LOGGER else getLogger(default_name)
